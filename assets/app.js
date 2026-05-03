661cdfbb2e3ee7308aa9e8319e1ee1006e83d1f3
Emival Santos <emvictorsantos@gmail.com>
2026-05-03 20:00:29 -0300

    Remove components js desnecessários

--- assets/app.js
+++
@@ -1,4 +0,0 @@
-import './components/action-card.js';
-import './components/action-button.js';
-import './components/price-field.js';
-import './components/modal-button.js';
--- importmap.php
+++ importmap.php
@@ -12,10 +12,6 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'app' => [
-        'path' => './assets/app.js',
-        'entrypoint' => true,
-    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
