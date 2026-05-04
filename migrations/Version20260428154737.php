dcf4f9f47608c510b3f182ce20fe50aff53b56b4
Emival Santos <emvictorsantos@gmail.com>
2026-05-03 21:36:52 -0300

    Ajusta migration original

--- migrations/Version20260428154737.php
+++ migrations/Version20260428154737.php
@@ -20,7 +20,7 @@ final class Version20260428154737 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE assunto (id INT AUTO_INCREMENT NOT NULL, desdescricao VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE assunto (id INT AUTO_INCREMENT NOT NULL, descricao VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE autor (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(40) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE livro (id INT AUTO_INCREMENT NOT NULL, titulo VARCHAR(40) NOT NULL, editora VARCHAR(40) NOT NULL, edicao INT NOT NULL, ano_publicacao VARCHAR(4) NOT NULL, valor NUMERIC(10, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE livro_autor (livro_id INT NOT NULL, autor_id INT NOT NULL, INDEX IDX_67499925864C5AF (livro_id), INDEX IDX_674999214D45BBE (autor_id), PRIMARY KEY(livro_id, autor_id)) DEFAULT CHARACTER SET utf8mb4');
